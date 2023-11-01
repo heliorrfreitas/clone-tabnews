797e84fa7618604ef5173cc89bb1090e3ea5ac7c
Helio Rui Ramos Freitas <helio2rfreitas@gmail.com>
2023-11-01 08:37:44 -0500

    Add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você é incrível, te amo! &#128536; &#128513; &#128170;</h1>
+  return <h1>Você é incrível, te amo! &#128536; &#128513; &#128170;</h1>;
 }
 
 export default Home;
