85955515c796a80f223d0a030c2335855e21c478
Helio Rui Ramos Freitas <helio2rfreitas@gmail.com>
2024-03-07 15:56:21 +0000

    Make   more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,12 +9,22 @@ async function query(queryObject){
         password: process.env.POSTGRES_PASSWORD
     });
 
+    console.log({
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD
+    });
+
     try{ 
         await client.connect();
         return await client.query(queryObject);
     }catch(error){
-        console.error(error)
+        console.error(error);
+        throw error;
     }finally{
+        console.log("aqui")
         await client.end();
     }
 }
